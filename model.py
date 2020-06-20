2b1d73585bc80f08715f9890983ac40ddb86e43f
Ganhart <ganhart2@gmail.com>
2020-06-20 22:08:20 +0300

    added additional methods to model.py

--- model.py
+++ model.py
@@ -2,13 +2,13 @@ from settings import DB_CONNECT
 from sqlalchemy import create_engine
 
 
-# engine = sqlalchemy.create_engine('postgresql:///atbdb')
-# connection = engine.connect()
-# metadata = sqlalchemy.MetaData()
-
-
 class DbUtils:
     db_string = DB_CONNECT
+
+    def DropTables(self):
+        db = create_engine(self.db_string)
+
+        db.execute("DROP TABLE users, assets")
 
     def InitTables(self):
         db = create_engine(self.db_string)
@@ -22,10 +22,10 @@ class DbUtils:
         db.execute("CREATE TABLE IF NOT EXISTS "
                    "assets (id int, user_id bigint, "
                    "ticker text, is_crypto boolean, "
-                   "add_date boolean, initial_price float, "
+                   "add_date date, initial_price float, "
                    "target_price float, min_price float)")
 
-    def addUser(self, id, user_id, shares_user,
+    def AddUser(self, id, user_id, shares_user,
                 crypto_user, analytics_subscription,
                 post_frequency, analytics_frequency, api_key):
 
@@ -38,38 +38,91 @@ class DbUtils:
                    shares_user, crypto_user, analytics_subscription,
                    post_frequency, analytics_frequency, api_key)
 
-    def getUsers(self):
+    def AddAsset(self, id, user_id, ticker,
+                 is_crypto, add_date, initial_price,
+                 target_price, min_price):
+
         db = create_engine(self.db_string)
-        users = db.execute("SELECT * FROM users")
+
+        db.execute("INSERT INTO assets(id, user_id, ticker, "
+                   "is_crypto, add_date, initial_price, "
+                   "target_price, min_price) "
+                   "VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
+                   id, user_id, ticker,
+                   is_crypto, add_date, initial_price,
+                   target_price, min_price)
+
+    def DeleteUser(self, user_id):
+        db = create_engine(self.db_string)
+
+        db.execute(f"DELETE FROM users WHERE user_id = {user_id}")
+
+    def DeleteAsset(self, user_id, ticker):
+        db = create_engine(self.db_string)
+
+        db.execute("DELETE FROM assets WHERE "
+                   f"user_id = {user_id} AND ticker = '{ticker}'")
+
+    def GetUserAssets(self, user_id):
+        db = create_engine(self.db_string)
+
+        assets = db.execute(f"SELECT * FROM assets WHERE user_id = {user_id}")
+
+        return assets
+
+    def GetSharesUsers(self):
+        db = create_engine(self.db_string)
+
+        users = db.execute("SELECT * FROM users WHERE shares_user = True")
+
         return users
+
+    def GetCryptoUsers(self):
+        db = create_engine(self.db_string)
+
+        users = db.execute("SELECT * FROM users WHERE crypto_user = True")
+
+        return users
+
+    def GetAnalyticsUsers(self):
+        db = create_engine(self.db_string)
+
+        users = db.execute("SELECT * FROM users WHERE "
+                           "analytics_subscription = True")
+
+        return users
+
+    def GetUsers(self):
+        db = create_engine(self.db_string)
+
+        users = db.execute("SELECT * FROM users")
+
+        return users
+
+    def GetAssets(self):
+        db = create_engine(self.db_string)
+
+        assets = db.execute("SELECT * FROM assets")
+
+        return assets
+
+    def EditUser(self, user_id, field, variable):
+        db = create_engine(self.db_string)
+
+        if field == 'api_key':
+            assets = db.execute(f"UPDATE users SET {field} = '{variable}'"
+                                f" WHERE user_id = {user_id}")
+        else:
+            assets = db.execute(f"UPDATE users SET {field} = {variable}"
+                                f" WHERE user_id = {user_id}")
+
+    def EditAsset(self, user_id, ticker, field, variable):
+        db = create_engine(self.db_string)
+
+        db.execute(f"UPDATE assets SET {field} = {variable}"
+                   f" WHERE user_id = {user_id} and ticker = '{ticker}'")
 
 
 # can = DbUtils()
+# can.DropTables()
 # can.InitTables()
-# can.addUser(1,123123,True,True,True,1,2,"somekey")
-# users = can.getUsers()
-# for user in users:
-#     print(user)
-
-'''
-class Users(db.Model):
-    id = db.Column(db.int)
-    user_id = db.Column(db.bigint)
-    shares_user = db.Column(db.boolean)
-    crypto_user = db.Column(db.boolean)
-    analytics_subscription = db.Column(db.boolean)
-    post_frequency = db.Column(db.int)
-    analytics_frequency = db.Column(db.int)
-    api_key = db.Column(db.text)
-
-
-class Assets(db.Model):
-    id = db.Column(db.int)
-    user_id = db.Column(db.bigint)
-    ticker = db.Column(db.text)
-    is_crypto = db.Column(db.boolean)
-    add_date = db.Column(db.date)
-    initial_price = db.Column(db.float)
-    target_price = db.Column(db.float)
-    min_price = db.Column(db.float)
-'''
