b41134aede09e59bc6ac1f7674ba4132d9b1d713
Stepan Bubnov <exectrogod@gmail.com>
2020-06-21 15:20:07 +0300

    Added list of methods for orders

--- binance_utils.py
+++ binance_utils.py
@@ -1,32 +1,77 @@
 from binance.client import Client
-from binance.exceptions import *
 from binance.enums import *
-from settings import *
+from settings import API_KEY, SECRET_KEY, EXCEPTION_LIST
+from pprint import PrettyPrinter
 
 
 
-binance_client = Client(API_KEY, SECRET_KEY)
+client = Client(API_KEY, SECRET_KEY)
+pp = PrettyPrinter()
 
 
 def set_order(ticket_1,ticket_2):
-    """" Задаём пользовательские аргументы вызову метода. """
-
+    """" Выставляет ордер с заданными параметрами. """
     
     try:
-        order = binance_client.create_test_order(
+        order = client.create_test_order(
         symbol=f'{ticket_1}{ticket_2}',
         side=SIDE_BUY,
         type=ORDER_TYPE_LIMIT,
         timeInForce=TIME_IN_FORCE_GTC,
         quantity=1,
         price=9800)
-    except (BinanceRequestException, BinanceAPIException, BinanceOrderException, 
-            BinanceOrderMinAmountException, BinanceOrderMinPriceException,
-            BinanceOrderMinTotalException, BinanceOrderUnknownSymbolException,
-            BinanceOrderInactiveSymbolException) as e:
-        print (f"К сожалению, возникла ошибка {e}. Попробуйте ещё раз.")
+    except EXCEPTION_LIST as e:
+        print (f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")
     else:
         print ("Выполнено.")
-        
+        return order # 
+
+
+def get_historical_data():
+    pass
+    #historical_data(client.get_recent_trades(symbol='BTTBTC'))
+
+    
+def get_average_price(ticket_1,ticket_2):
+    """ Возвращает текущую цену заданной пары. """
+
+    try:
+        avg_price = client.get_avg_price(symbol=f'{ticket_1}{ticket_2}')
+    except EXCEPTION_LIST as ex:
+        print (f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
+    else:
+        return avg_price['price'] + f' {ticket_2}'
+
+
+def get_all_open_orders():
+    """ Возвращает список открытых сделок. """
+
+    try:
+        orders = client.get_open_orders()
+    except EXCEPTION_LIST as ex:
+        print (f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
+    else:
+        return orders
+
+
+def close_order(ticket_1,ticket_2,orderId):
+    """ Закрывает ордер. """
+
+    orders = get_all_open_orders()  #Проверяем если ли ордер на закрытие. 
+    for order in orders:                      
+        if order.get('sybmol') == ticket_1 and order.get('sybmol') == ticket_1 and ordet.get('orderId') == orderId:
+            try:
+                result = cancel_order(symbol=f'{ticket_1}{ticket_2}',orderId='orderId')
+                print("Ордер успешно закрыт.")
+                return result
+            except EXCEPTION_LIST as ex:
+                print (f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
+        else:
+            return 'К сожалению, нет ордеров на закрытие.'
+
+
+
+
+    
     
 
--- client_app.py
+++ client_app.py
@@ -1,9 +1,20 @@
 from settings import *
 from binance_utils import *
+from pprint import PrettyPrinter
+
+
+pp = PrettyPrinter()
+
 
 def main():
 
     set_order('BTC','USDT')
+    print(get_average_price('ETC','USDT'))
+    print(get_average_price('BTC','USDT'))
+    print(get_all_open_orders())
+    #pp.pprint(close_order('BTC','USDT',SOME_ORDER_ID)) # Проверить можно только с реальным ордером.
+    
+    
     
 
 
