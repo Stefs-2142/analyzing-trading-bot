c8fc17518245c0da6b567612e9eeaa870df969e6
Ganhart <ganhart2@gmail.com>
2020-06-21 19:15:29 +0300

    handlers_utils style fixed

--- handlers_utils.py
+++ handlers_utils.py
@@ -1,10 +1,12 @@
 ﻿# Функция-обработчик команды /start
 def greet_user(update, context):
     # Дописать нормальный текст
-    update.message.reply_text('Привет!')
-    
+    update.message.reply_text("Привет!")
+
+
 # Функция-обработчик команды /start
 def unknown_text(update, context):
     # Дописать нормальный текст
-    reply = 'Команда не найдена, пожалуйста, нажмите на одну из доступных кнопок'
-    update.message.reply_text(reply)    
+    reply = ("Команда не найдена, пожалуйста, "
+             "нажмите на одну из доступных кнопок")
+    update.message.reply_text(reply)
