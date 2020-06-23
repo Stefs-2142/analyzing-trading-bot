dfde49367bb288e7983b634f3310a0b4865372d8
Stepan Bubnov <exectrogod@gmail.com>
2020-06-24 00:29:28 +0300

    added flake8 and fix errors

--- binance_utils.py
+++ binance_utils.py
@@ -1,5 +1,5 @@
 from binance.client import Client
-from binance.enums import *
+from binance.enums import SIDE_BUY, ORDER_TYPE_LIMIT, TIME_IN_FORCE_GTC
 from settings import API_KEY, SECRET_KEY, EXCEPTION_LIST
 from pprint import PrettyPrinter
 
@@ -8,7 +8,7 @@ client = Client(API_KEY, SECRET_KEY)
 pp = PrettyPrinter()
 
 
-def set_order(ticket_1,ticket_2):
+def set_order(ticket_1, ticket_2):
     """" Выставляет ордер с заданными параметрами. """
     
     try:
@@ -19,20 +19,20 @@ def set_order(ticket_1,ticket_2):
         timeInForce=TIME_IN_FORCE_GTC,
         quantity=1,
         price=9800)
-    except EXCEPTION_LIST as e:
-        print (f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")
+    except EXCEPTION_LIST as ex:
+        print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")
     else:
-        print ("Выполнено.")
+        print("Выполнено.")
         return order 
 
 
-def get_average_price(ticket_1,ticket_2):
+def get_average_price(ticket_1, ticket_2):
     """ Возвращает текущую цену заданной пары. """
 
     try:
-        avg_price = client.get_avg_price(symbol=f'{ticket_1}{ticket_2}') # Получаем среднее значение цены за 5 мин.
+        avg_price = client.get_avg_price(symbol=f'{ticket_1}{ticket_2}')  # Получаем среднее значение цены за 5 мин.
     except EXCEPTION_LIST as ex:
-        print (f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
+        print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
     else:
         return avg_price['price'] + f' {ticket_2}'
 
@@ -41,25 +41,25 @@ def get_all_open_orders():
     """ Возвращает список открытых сделок. """
 
     try:
-        orders = client.get_open_orders() # Получаем список открытых сделок.
+        orders = client.get_open_orders()  # Получаем список открытых сделок.
     except EXCEPTION_LIST as ex:
-        print (f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
+        print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
     else:
         return orders
 
 
-def close_order(ticket_1,ticket_2,orderId):
+def close_order(ticket_1, ticket_2, orderId):
     """ Закрывает ордер. """
 
     orders = get_all_open_orders()  # Проверяем есть ли ордер на закрытие. 
-    for order in orders:                      
-        if order.get('sybmol') == ticket_1 and order.get('sybmol') == ticket_1 and ordet.get('orderId') == orderId:
+    for order in orders:                       
+        if order.get('symbol') == ticket_1 and order.get('symbol') == ticket_1 and order.get('orderId') == orderId:
             try:
-                result = cancel_order(symbol=f'{ticket_1}{ticket_2}',orderId='orderId')
+                result = client.cancel_order(symbol=f'{ticket_1}{ticket_2}', orderId='orderId')
                 print("Ордер успешно закрыт.")
                 return result
             except EXCEPTION_LIST as ex:
-                print (f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
+                print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")  
         else:
             return 'К сожалению, нет ордеров на закрытие.'
 
@@ -68,10 +68,11 @@ def get_balance():
     """ Возвращает баланс пользователя. """
 
     info = client.get_account()
-    balance = {}                        # Создаём словарь с балансом пользователя. {'BTC': 2, 'ETC': 12}
+    balance = {}  # Создаём словарь с балансом пользователя.{'BTC':2, 'ETC':12}
     for crypto in info['balances']:
         if crypto['free'] != '0.00000000':
             balance[crypto['asset']] = crypto['free']
     return balance
+
 
 
