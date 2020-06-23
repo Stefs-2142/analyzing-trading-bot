c439665b9c7e7872873f0617351fefc8e78c97fe
Stepan Bubnov <exectrogod@gmail.com>
2020-06-24 01:09:18 +0300

    Created BinanceClient class

--- binance_utils.py
+++ binance_utils.py
@@ -8,71 +8,74 @@ client = Client(API_KEY, SECRET_KEY)
 pp = PrettyPrinter()
 
 
-def set_order(ticket_1, ticket_2):
-    """" Выставляет ордер с заданными параметрами. """
-    
-    try:
-        order = client.create_test_order(  # Создаём тестовый ордер в тестовой сети.
-        symbol=f'{ticket_1}{ticket_2}',
-        side=SIDE_BUY,
-        type=ORDER_TYPE_LIMIT,
-        timeInForce=TIME_IN_FORCE_GTC,
-        quantity=1,
-        price=9800)
-    except EXCEPTION_LIST as ex:
-        print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")
-    else:
-        print("Выполнено.")
-        return order 
+class BinanceClient():
+
+    def __init__(self):
+        self.client = Client(API_KEY, SECRET_KEY)
 
 
-def get_average_price(ticket_1, ticket_2):
-    """ Возвращает текущую цену заданной пары. """
+    def set_order(self, ticket_1, ticket_2):
+        """" Выставляет ордер с заданными параметрами. """
 
-    try:
-        avg_price = client.get_avg_price(symbol=f'{ticket_1}{ticket_2}')  # Получаем среднее значение цены за 5 мин.
-    except EXCEPTION_LIST as ex:
-        print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
-    else:
-        return avg_price['price'] + f' {ticket_2}'
-
-
-def get_all_open_orders():
-    """ Возвращает список открытых сделок. """
-
-    try:
-        orders = client.get_open_orders()  # Получаем список открытых сделок.
-    except EXCEPTION_LIST as ex:
-        print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
-    else:
-        return orders
-
-
-def close_order(ticket_1, ticket_2, orderId):
-    """ Закрывает ордер. """
-
-    orders = get_all_open_orders()  # Проверяем есть ли ордер на закрытие. 
-    for order in orders:                       
-        if order.get('symbol') == ticket_1 and order.get('symbol') == ticket_1 and order.get('orderId') == orderId:
-            try:
-                result = client.cancel_order(symbol=f'{ticket_1}{ticket_2}', orderId='orderId')
-                print("Ордер успешно закрыт.")
-                return result
-            except EXCEPTION_LIST as ex:
-                print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")  
+        try:
+            order = client.create_test_order(  # Создаём тестовый ордер в тестовой сети.
+            symbol=f'{ticket_1}{ticket_2}',
+            side=SIDE_BUY,
+            type=ORDER_TYPE_LIMIT,
+            timeInForce=TIME_IN_FORCE_GTC,
+            quantity=1,
+            price=9800)
+        except EXCEPTION_LIST as ex:
+            print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")
         else:
-            return 'К сожалению, нет ордеров на закрытие.'
+            print("Выполнено.")
+            return order
 
 
-def get_balance():
-    """ Возвращает баланс пользователя. """
+    def get_average_price(self, ticket_1, ticket_2):
+        """ Возвращает текущую цену заданной пары. """
 
-    info = client.get_account()
-    balance = {}  # Создаём словарь с балансом пользователя.{'BTC':2, 'ETC':12}
-    for crypto in info['balances']:
-        if crypto['free'] != '0.00000000':
-            balance[crypto['asset']] = crypto['free']
-    return balance
+        try:
+            avg_price = client.get_avg_price(symbol=f'{ticket_1}{ticket_2}')  # Получаем среднее значение цены за 5 мин.
+        except EXCEPTION_LIST as ex:
+            print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.") 
+        else:
+            return avg_price['price'] + f' {ticket_2}'
 
 
+    def get_all_open_orders(self):
+        """ Возвращает список открытых сделок. """
 
+        try:
+            orders = client.get_open_orders()  # Получаем список открытых сделок.
+        except EXCEPTION_LIST as ex:
+            print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")
+        else:
+            return orders
+
+
+    def close_order(self, ticket_1, ticket_2, orderId):
+        """ Закрывает ордер. """
+
+        orders = self.get_all_open_orders()  # Проверяем есть ли ордер на закрытие. 
+        for order in orders:
+            if order.get('symbol') == ticket_1 and order.get('symbol') == ticket_1 and order.get('orderId') == orderId:
+                try:
+                    result = client.cancel_order(symbol=f'{ticket_1}{ticket_2}', orderId='orderId')
+                    print("Ордер успешно закрыт.")
+                    return result
+                except EXCEPTION_LIST as ex:
+                    print(f"К сожалению, возникла ошибка {ex}. Попробуйте ещё раз.")  
+            else:
+                return 'К сожалению, нет ордеров на закрытие.'
+
+
+    def get_balance(self):
+        """ Возвращает баланс пользователя. """
+
+        info = client.get_account()
+        balance = {}  # Создаём словарь с балансом пользователя.{'BTC':2, 'ETC':12}
+        for crypto in info['balances']:
+            if crypto['free'] != '0.00000000':
+                balance[crypto['asset']] = crypto['free']
+        return balance
--- client_app.py
+++ client_app.py
@@ -1,5 +1,5 @@
 from settings import *
-from binance_utils import *
+from binance_utils import BinanceClient
 from pprint import PrettyPrinter
 
 
@@ -8,15 +8,15 @@ pp = PrettyPrinter()
 
 def main():
 
-    set_order('BTC','USDT')
-    print(get_average_price('ETC','USDT'))
-    print(get_average_price('BTC','USDT'))
-    print(get_all_open_orders())
-    print(get_balance())
-    
-    #pp.pprint(close_order('BTC','USDT',SOME_ORDER_ID)) # Проверить можно только с реальным ордером.
+    binance_client = BinanceClient()
+    binance_client.set_order('BTC', 'USDT')
+    print(binance_client.get_average_price('ETC', 'USDT'))
+    print(binance_client.get_average_price('BTC', 'USDT'))
+    print(binance_client.get_all_open_orders())
+    print(binance_client.get_balance())
+
+    # pp.pprint(close_order('BTC','USDT',SOME_ORDER_ID)) # Проверить можно только с реальным ордером.
 
 
 if __name__ == "__main__":
     main()
-    
