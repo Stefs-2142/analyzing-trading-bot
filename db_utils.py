688b5dde9d58bf92547eb1b8c2e48e81b374cdbe
Ganhart <ganhart2@gmail.com>
2020-06-24 18:47:20 +0300

    model reworked, added example of settings

---
+++ db_utils.py
@@ -0,0 +1,43 @@
+﻿from base import Session, Engine, Base
+from model import User, Asset
+
+Base.metadata.create_all(Engine)
+
+def add_user(user_data):
+    session = Session()
+    
+    users = session.query(User).all()
+    
+    if not [user for user in users if user.user_id == user_data[0]]:
+        session.add(User(*user_data))
+        session.commit()
+        return True
+    else:
+        session.close()
+        return False
+
+
+def add_asset(asset_data):
+    session = Session()
+    
+    assets = session.query(Asset).all()
+    
+    if not [asset for asset in assets if asset.user_id == asset_data[0] and asset.ticker == asset_data[1]]:
+        session.add(Asset(*asset_data))
+        session.commit()
+        return True
+    else:
+        session.close()
+        return False        
+        
+        
+print(add_user([33332,False,True,True,1,2,"somekey"]))
+print(add_asset([24124,"RSTI",False,'2001-09-28',5.2,6.7,4.2]))    
+
+session = Session()
+users = session.query(User).all()
+assets = session.query(Asset).all()
+
+for asset in assets:
+    print(asset.user_id, asset.ticker)
+
--- model.py
+++ model.py
@@ -1,128 +1,54 @@
-from settings import DB_CONNECT
-from sqlalchemy import create_engine
+from base import Base
+from sqlalchemy import create_engine, Column
+from sqlalchemy import BigInteger, Boolean, Integer, Text, Date, Float
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class DbUtils:
-    db_string = DB_CONNECT
-
-    def DropTables(self):
-        db = create_engine(self.db_string)
-
-        db.execute("DROP TABLE users, assets")
-
-    def InitTables(self):
-        db = create_engine(self.db_string)
-
-        db.execute("CREATE TABLE IF NOT EXISTS "
-                   "users (id int, user_id bigint, "
-                   "shares_user boolean, crypto_user boolean, "
-                   "analytics_subscription boolean, post_frequency int, "
-                   "analytics_frequency int, api_key text)")
-
-        db.execute("CREATE TABLE IF NOT EXISTS "
-                   "assets (id int, user_id bigint, "
-                   "ticker text, is_crypto boolean, "
-                   "add_date date, initial_price float, "
-                   "target_price float, min_price float)")
-
-    def AddUser(self, id, user_id, shares_user,
-                crypto_user, analytics_subscription,
-                post_frequency, analytics_frequency, api_key):
-
-        db = create_engine(self.db_string)
-
-        db.execute("INSERT INTO users(id, user_id, shares_user, crypto_user, "
-                   "analytics_subscription, post_frequency, "
-                   "analytics_frequency, api_key) "
-                   "VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", id, user_id,
-                   shares_user, crypto_user, analytics_subscription,
-                   post_frequency, analytics_frequency, api_key)
-
-    def AddAsset(self, id, user_id, ticker,
-                 is_crypto, add_date, initial_price,
-                 target_price, min_price):
-
-        db = create_engine(self.db_string)
-
-        db.execute("INSERT INTO assets(id, user_id, ticker, "
-                   "is_crypto, add_date, initial_price, "
-                   "target_price, min_price) "
-                   "VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
-                   id, user_id, ticker,
-                   is_crypto, add_date, initial_price,
-                   target_price, min_price)
-
-    def DeleteUser(self, user_id):
-        db = create_engine(self.db_string)
-
-        db.execute(f"DELETE FROM users WHERE user_id = {user_id}")
-
-    def DeleteAsset(self, user_id, ticker):
-        db = create_engine(self.db_string)
-
-        db.execute("DELETE FROM assets WHERE "
-                   f"user_id = {user_id} AND ticker = '{ticker}'")
-
-    def GetUserAssets(self, user_id):
-        db = create_engine(self.db_string)
-
-        assets = db.execute(f"SELECT * FROM assets WHERE user_id = {user_id}")
-
-        return assets
-
-    def GetSharesUsers(self):
-        db = create_engine(self.db_string)
-
-        users = db.execute("SELECT * FROM users WHERE shares_user = True")
-
-        return users
-
-    def GetCryptoUsers(self):
-        db = create_engine(self.db_string)
-
-        users = db.execute("SELECT * FROM users WHERE crypto_user = True")
-
-        return users
-
-    def GetAnalyticsUsers(self):
-        db = create_engine(self.db_string)
-
-        users = db.execute("SELECT * FROM users WHERE "
-                           "analytics_subscription = True")
-
-        return users
-
-    def GetUsers(self):
-        db = create_engine(self.db_string)
-
-        users = db.execute("SELECT * FROM users")
-
-        return users
-
-    def GetAssets(self):
-        db = create_engine(self.db_string)
-
-        assets = db.execute("SELECT * FROM assets")
-
-        return assets
-
-    def EditUser(self, user_id, field, variable):
-        db = create_engine(self.db_string)
-
-        if field == 'api_key':
-            assets = db.execute(f"UPDATE users SET {field} = '{variable}'"
-                                f" WHERE user_id = {user_id}")
-        else:
-            assets = db.execute(f"UPDATE users SET {field} = {variable}"
-                                f" WHERE user_id = {user_id}")
-
-    def EditAsset(self, user_id, ticker, field, variable):
-        db = create_engine(self.db_string)
-
-        db.execute(f"UPDATE assets SET {field} = {variable}"
-                   f" WHERE user_id = {user_id} and ticker = '{ticker}'")
+class User(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key = True)
+    user_id = Column('user_id', BigInteger)
+    shares_user = Column('shares_user', Boolean)
+    crypto_user = Column('crypto_user', Boolean)
+    analytics_subscription = Column('analytics_subscription', Boolean)
+    post_frequency = Column('post_frequency', Integer)
+    analytics_frequency = Column('analytics_frequency', Integer)
+    api_key = Column('api_key', Text)
+        
+    def __init__(self, user_id, shares_user, crypto_user,
+                analytics_subscription, post_frequency, 
+                analytics_frequency, api_key):
+                
+        self.user_id = user_id
+        self.shares_user = shares_user
+        self.crypto_user = crypto_user
+        self.analytics_subscription = analytics_subscription
+        self.post_frequency = post_frequency
+        self.analytics_frequency = analytics_frequency
+        self.api_key = api_key
 
 
-# can = DbUtils()
-# can.DropTables()
-# can.InitTables()
+class Asset(Base):
+    __tablename__ = 'asset'
+    
+    id = Column(Integer, primary_key = True)
+    user_id = Column('user_id', BigInteger)
+    ticker = Column('ticker', Text)
+    is_crypto = Column('is_crypto', Boolean)
+    add_date = Column('add_date', Date)
+    initial_price = Column('initial_price', Float)
+    target_price = Column('target_price', Float)
+    min_price = Column('min_price', Float)
+        
+    def __init__(self, user_id, ticker, is_crypto,
+                add_date, initial_price, 
+                target_price, min_price):
+                
+        self.user_id = user_id
+        self.ticker = ticker
+        self.is_crypto = is_crypto
+        self.add_date = add_date
+        self.initial_price = initial_price
+        self.target_price = target_price
+        self.min_price = min_price
