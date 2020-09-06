180d4a695695a75cc7ca5d244ece1a1379d368aa
Stepan Bubnov <exectrogod@gmail.com>
2020-09-06 18:45:50 +0300

    updated close_order handler

--- handlers_close_order.py
+++ handlers_close_order.py
@@ -1,44 +1,67 @@
+from telegram.ext import ConversationHandler
+
+from handlers_utils import clear_all_crypto
+
 from binance_utils import binance_client
 from keyboards import numbers_keyboard, yes_no_keyboard
 from keyboards import main_menu_keyboard
-from keyboards import NUMBERS
-from telegram.ext import ConversationHandler
 
 
 def choosing_order_for_close(update, context):
     """Спрашиваем у пользователя какой ордер закрыть."""
 
+    # Получаем список открытых ордеров.
     open_orders = binance_client.get_all_open_orders()
 
-    # Сохраняем открытые ордера.
-    context.user_data['open_orders'] = open_orders
+    if open_orders:
+        # Сохраняем открытые ордера.
+        context.user_data['open_orders'] = open_orders
+        formated_orders = ""
+        order_count = 1
+        for order in open_orders:
+            formated_orders += f"{order_count}. {order['symbol']}"
+            formated_orders += f" {order['type']} {order['side']}"
+            formated_orders += f" {order['price']}\n"
+            order_count += 1
+            # Cохраняем количество отрктых ордеров.
+            context.user_data['total_open_orders'] = order_count
 
-    formated_orders = ""
-    order_count = 1
-    for order in open_orders:
-        formated_orders += f"{order_count}. {order['symbol']}"
-        formated_orders += f" {order['type']} {order['side']}"
-        formated_orders += f" {order['price']}\n"
-        order_count += 1
+        update.message.reply_text(
+            f"{formated_orders}\nВыберите ордер для закрытия.",
+            reply_markup=numbers_keyboard(order_count)
+            )
+        return 'close_step_1'
 
+    # Если нет открытых ордеров.
     update.message.reply_text(
-        f"{formated_orders}\nВыберите ордер для закрытия.",
-        reply_markup=numbers_keyboard(order_count)
+        'Нет открытых ордеров.', keyboard_markup=main_menu_keyboard()
         )
-    return 'close_step_1'
+    clear_all_crypto()
+    return ConversationHandler.END
 
 
 def applying_closing(update, context):
     """Подтверждаем закрытие оредра."""
 
-    if update.message.text not in NUMBERS:
+    order_count = context.user_data['total_open_orders']
+    # Проверяем введённые данные на валидность.
+    try:
+        user_text = int(update.message.text)
+    except (TypeError, ValueError):
+        update.message.reply_text(
+            'Выберите, пожалуйста, ордер для закрытия.',
+            keyboard_markup=numbers_keyboard(order_count)
+            )
+        return 'close_step_1'
+    # Проверяем введённые данные на валидность.
+    if user_text not in range(1, order_count):
         update.message.reply_text(
             'Пожалуйста, выберите, ордер для закрытия или нажмите "Отмена"'
         )
         return 'close_step_1'
 
-    # Сохраняем в выбранный ордер для закрытия.
-    context.user_data['order_count_for_close'] = int(update.message.text)
+    # Сохраняем выбранный ордер для закрытия.
+    context.user_data['order_count_for_close'] = user_text
 
     update.message.reply_text(
         'Отменить ордер?', reply_markup=yes_no_keyboard()
