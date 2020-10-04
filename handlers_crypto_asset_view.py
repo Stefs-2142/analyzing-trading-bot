81f5fb0dada9b5ef3016343abd45ee0e4c03622c
Stepan Bubnov <exectrogod@gmail.com>
2020-10-05 01:41:45 +0300

    added crypto_asset_view handler

---
+++ handlers_crypto_asset_view.py
@@ -0,0 +1,54 @@
+from models import Asset
+
+from binance_utils import binance_client
+
+
+def crypto_asset_view(update, context):
+    """
+    Выгружаем все отслеживаемые пользователем инструменты из БД
+    """
+    user_assets = Asset().get_user_assets(update.effective_user.id)
+    # Пересобираем список оставляя крипто-активы.
+    user_assets = [asset for asset in user_assets if asset[5] is True]
+    if user_assets:
+        update.message.reply_text('Сейчас отслеживаются следующие инструменты:')
+        """
+        Циклом проходим по каждому инструменту:
+        Получаем текущую стоимость актива и стоимость последнего закрытия
+        Добавляем эти две стоимости в список, который получили из БД и
+        передаем его в соседнюю функцию для компиляции сообщений пользователю
+        """
+        for asset in user_assets:
+            reply = compile_message(asset)
+            update.message.reply_text(reply)
+    else:
+        update.message.reply_text('Ещё нет отслеживаемых инструментов.')
+
+
+def compile_message(asset):
+    (
+        ticker, add_date, initial_price, target_price, min_price, current_price
+    ) = asset
+    current_price = float(binance_client.get_average_price(
+        ticker.split('/')[0], ticker.split('/')[1]
+        ).split(' ')[0])
+    reply = f'Идентификатор инструмента:\r\n{ticker}\r\n\r\n'
+    reply += f'Начальная цена:\r\n{initial_price}\r\n\r\n'
+    reply += f'Текущая цена:\r\n{current_price}\r\n\r\n'
+    if target_price != 0:
+        reply += f'Целевая стоимость:\r\n{target_price}\r\n\r\n'
+    if min_price != 0:
+        reply += f'Минимальная стоимость:\r\n{min_price}\r\n\r\n'
+    """
+    Считаем процент изменение цены с начала отслеживания (т.е. от
+    цены, которая была на момент когда пользователь добавил себе
+    этот инструмент)
+    """
+    overall_change = round(
+        ((initial_price - current_price) * 100 / initial_price) * -1, 3
+    )
+    reply += f'Изменение цены за всё время:\r\n{overall_change}%\r\n\r\n'
+    """
+    Считаем процент изменения цены за сегодня, т.е. от цены последнего закрытия
+    """
+    return reply
