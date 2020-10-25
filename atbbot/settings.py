1cd702d3142d7bab69f66f289300334b5b9eab28
Stepan Bubnov <exectrogod@gmail.com>
2020-10-25 18:38:57 +0300

    added DB_URL

--- atbbot/settings.py
+++ atbbot/settings.py
@@ -1,7 +1,9 @@
+import os
+
 from binance.exceptions import *
 
 TELEGRAM_API_KEY = "1273602121:AAHyItK7iNe6sx7iMpSG6mgknM9Or7NTgxg"
-DB_CONNECT = "postgresql:///atbdb"
+DB_CONNECT = os.environ['DATABASE_URL']
 BACKEND_PATH = "rpc://"
 BROKER_PATH = "redis://localhost:6379/0"
 BINANCE_API_KEY = 'cywfdaMkZBSUgWm3H1rDqg9jMzAUXIyiAQpybBvsoKIlSTuMYIqEGojKfUYZq9Kp'
